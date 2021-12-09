160fc513b1f0dfab47dead67f161be664ab27a91
kingcode <samuelvilorio_15@hotmail.com>
2021-12-09 17:42:25 -0400

    feat: added auth and index file. set server basics

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+//import routes
+const authRoute = require('./routes/auth');
+
+//Route middleware
+app.use('/api/user', authRoute)
+
 app.listen(3000, () => console.log("Server's up!"));
