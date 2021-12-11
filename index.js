48e55d769a2a6936d1e7539784ee8e7b066d16bd
kingcode <samuelvilorio_15@hotmail.com>
2021-12-10 23:55:30 -0400

    feat: added connection to DB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
+//connect to db
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> console.log('Connected to DB'));
 //import routes
 const authRoute = require('./routes/auth');
 
