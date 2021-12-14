68a023feb01c82ff26f554528972f5512e59b49c
kingcode <samuelvilorio_15@hotmail.com>
2021-12-13 21:45:39 -0400

    feat: added user model, inserts records to table

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
+//import routes
+const authRoute = require('./routes/auth');
 
 //connect to db
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> console.log('Connected to DB'));
-//import routes
-const authRoute = require('./routes/auth');
+//midleware
+app.use(express.json());
 
 //Route middleware
 app.use('/api/user', authRoute)
