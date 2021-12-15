41fa48174bcc292aecbab2b3fb75085415ee78bf
kingcode <samuelvilorio_15@hotmail.com>
2021-12-14 23:48:02 -0400

    added: a login function

--- validation.js
+++ validation.js
@@ -10,7 +10,12 @@ const registerValidation = (data) =>{
 }
 
 const loginValidation = (data) => {
-
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return schema.validate(data);
 }
 
 module.exports.registerValidation = registerValidation;
+module.exports.registerValidation = loginValidation;
