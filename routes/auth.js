b36439fbe3a24a3af4524fef76bdb6e8c7c9bbe0
kingcode <samuelvilorio_15@hotmail.com>
2021-12-14 23:46:26 -0400

    feat: added a validation file

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,7 @@ const router = require('express').Router();
 const User = require('../model/User');
 
 
-//Validations
-const Joi = require('@hapi/joi');
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-});
+
 router.post('/register', async (req, res) =>{
 
     //Validating data
---
+++ validation.js
@@ -0,0 +1,16 @@
+//Validations
+const Joi = require('@hapi/joi');
+const registerValidation = (data) =>{
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+    return schema.validate(data);
+}
+
+const loginValidation = (data) => {
+
+}
+
+module.exports.registerValidation = registerValidation;
